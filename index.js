77b4a44f5c4724e75554d6a14e592da1bb6690d4
dane <dane@rs2media.com>
2026-01-30 23:11:23 -0800

    back to ev

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ async function handleTrack(req, res) {
           event_name: ev.event_name || ev.event || null,
           event_id: ev.event_id || null,
           event_time: (ev.event_time || ev.timestamp) ? new Date(ev.event_time || ev.timestamp) : null,
-          raw: JSON.stringify(ev)
+          raw: ev
         });
       }
 
